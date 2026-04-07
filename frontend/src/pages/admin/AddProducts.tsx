8650e87d921223d138a6b6af8b709fae25cfc149
mz0018 <martinezhanzmenzi@gmail.com>
2026-04-07 20:24:48 +0800

    initial form inputs for product adding 201

--- frontend/src/pages/admin/AddProducts.tsx
+++ frontend/src/pages/admin/AddProducts.tsx
@@ -1,9 +1,53 @@
+import { useState } from 'react'
+import { Input } from '../../ui/form/Input'
+
+
+
 const AddProducts = () => {
+
+    const [files, setFiles] = useState<File[]>([])
+
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const input = e.currentTarget
+
+        if (!input.files) return
+
+        const selectedFiles = Array.from(input.files)
+
+        setFiles(prev => [...prev, ...selectedFiles])
+    }
+
+    const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const formData = new FormData(e.currentTarget)
+
+        files.forEach(file => formData.append('images', file))
+
+        console.log(Object.fromEntries(formData.entries()))
+        console.log(formData.getAll('images'))
+    }
 
     return (
         <>
             <h1>Add Product</h1>
             <p>This is the add product page.</p>
+
+            <form onSubmit={handleSubmit}>
+                <Input type='text' name='name' placeholder='Product Name' required />
+                <Input type='text' name='category' placeholder='ProductCategory' required />
+                <Input type='text' name='description' placeholder='Product Description' required />
+                <Input type='number' name='price' placeholder='Product Price' required />
+                <Input type='number' name='stock' placeholder='Product Stock' required />
+
+                <input type='file' name='images' accept='image/*' onChange={handleFileChange} multiple />
+
+                <button type='submit'>Add Product</button>
+            </form>
+
+            {files.map(file => (
+                <img key={file.name} src={URL.createObjectURL(file)} alt={file.name} width={100} />
+            ))}
         </>
     )
 }
