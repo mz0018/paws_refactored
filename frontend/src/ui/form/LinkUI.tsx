d9b6c9aa741b24cb279cd69521a34f5d8207243c
mz0018 <martinezhanzmenzi@gmail.com>
2026-04-20 20:15:57 +0800

    Link ui implemented 201

--- frontend/src/ui/form/LinkUI.tsx
+++ frontend/src/ui/form/LinkUI.tsx
@@ -8,4 +8,4 @@ export const LinkUI = ({ className = '', ...props }: LinkProps) => {
             className={`inline-block p-4 rounded-sm border bg-blue-500 text-white hover:bg-blue-700 w-1/2 text-center ${className}`}
         />
     )
-}
+}  
