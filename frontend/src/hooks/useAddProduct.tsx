955c9b77336247546a7dd17dcafa1e0d8606bbf1
mz0018 <martinezhanzmenzi@gmail.com>
2026-05-11 21:58:07 +0800

    removed unused useEffect 201

--- frontend/src/hooks/useAddProduct.tsx
+++ frontend/src/hooks/useAddProduct.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { productInputValidator } from '../utils/productInputValidator'
 import { useProductFiles } from './useProductFiles'
 
