1869f6d4d3b37cbcbef12a5c012bf8a81dd95565
mz0018 <martinezhanzmenzi@gmail.com>
2026-05-20 16:45:23 +0800

    initial design of appointment 201

--- frontend/src/pages/Appointment.tsx
+++ frontend/src/pages/Appointment.tsx
@@ -12,8 +12,26 @@ const Appointment = () => {
     const { formData, hasErrors, isLoading, handleChange, handleSubmit } = useAppointment()
 
     return (
-        <div className="min-h-screen flex items-center justify-center">
-            <Form onSubmit={handleSubmit}>
+        <div className="flex min-h-dvh items-center justify-center px-4 py-8 sm:px-6 lg:px-8">
+            <Form
+                onSubmit={handleSubmit}
+                className="w-full max-w-lg rounded-xl p-6 sm:p-8"
+            >
+
+                <div className="space-y-1 text-center sm:text-left">
+                    <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+                        Veterinary <span className="text-btn-black-bg">Appointment</span>
+                    </h1>
+
+                    <p className="text-md tracking-wide text-text-body">
+                        Book an appointment for veterinary consultation and services.
+                    </p>
+                </div>
+
+                <div className="my-2 relative">
+                    <div className="h-px w-full bg-text-body/10" />
+                    <div className="absolute inset-0 shadow-md" />
+                </div>
 
                 <Input
                     type="text"
@@ -49,13 +67,17 @@ const Appointment = () => {
 
                 <ErrorText message={hasErrors.name || hasErrors.selectedDate || hasErrors.purpose} />
 
-                <Button type="submit" className="bg-btn-black-bg text-white hover:bg-btn-black-hover-header-bg transition duration-300" disabled={isLoading}>
+                <Button
+                    type="submit"
+                    className="font-semibold tracking-wide mt-2 flex w-full items-center justify-center gap-2 bg-btn-black-bg hover:bg-btn-black-hover-header-bg py-3 text-white transition"
+                    disabled={isLoading}
+                >
                     {isLoading ? (
-                        <ClipLoader size={18} color='white' />
+                        <ClipLoader size={18} color="white" />
                     ) : (
                         <>
-                        <Send size={18} color='white' />
-                        Submit
+                            <Send size={18} />
+                            Submit
                         </>
                     )}
                 </Button>
