824d9d5f95a1e2490e042a98fd6a84b903f994ca
mz0018 <martinezhanzmenzi@gmail.com>
2026-05-29 16:50:32 +0800

    added max width on product overview 201

--- frontend/src/pages/ProductOverview.tsx
+++ frontend/src/pages/ProductOverview.tsx
@@ -63,7 +63,7 @@ const ProductOverview = () => {
                 <NotFound label='Products' />
             ) : (
                 <>
-                    <div className="p-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+                    <div className="max-w-7xl mx-auto p-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
                         {products.map((product) => (
                             <div key={product._id}>
                                 <ClientProductContainer key={product._id} product={product} />
--- frontend/src/ui/form/ClientProductContainer.tsx
+++ frontend/src/ui/form/ClientProductContainer.tsx
@@ -14,7 +14,7 @@ type ClientProductContainerProps = {
 
 export const ClientProductContainer = ({ product }: ClientProductContainerProps) => {
   return (
-    <div className="p-3 bg-white rounded-sm">
+    <div className="p-3 bg-white rounded-sm shadow-md">
       <Link to={`/detailed-product-overview/${product._id}`}>
         <Image
           src={product.images[0]?.url}
@@ -24,7 +24,7 @@ export const ClientProductContainer = ({ product }: ClientProductContainerProps)
         </Image>
       </Link>
 
-      <h3 className="mt-1 md:mt-2 uppercase text-text-body font-medium sm:font-semibold line-clamp-1 tracking-wide text-xs sm:text-sm md:text-base">
+      <h3 className="mt-1 md:mt-2 uppercase text-text-body font-medium sm:font-semibold line-clamp-1 tracking-wide text-xs sm:text-sm md:text-base leading-none">
         {product.productName}
       </h3>
     </div>
