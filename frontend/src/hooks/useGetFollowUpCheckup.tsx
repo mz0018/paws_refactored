43b2bede29ad1b806397ca7ed677474ccb543cc1
mz0018 <martinezhanzmenzi@gmail.com>
2026-06-23 20:27:24 +0800

    initial trial of get checkups 201

---
+++ frontend/src/hooks/useGetFollowUpCheckup.tsx
@@ -0,0 +1,9 @@
+
+export const useGetFollowUpCheckup = () => {
+
+    const handleGetFollowUpCheckUps = () => {
+
+    }
+
+    return { handleGetFollowUpCheckUps }
+}
--- frontend/src/pages/admin/FollowUpCheckup.tsx
+++ frontend/src/pages/admin/FollowUpCheckup.tsx
@@ -1,4 +1,8 @@
+import { useGetFollowUpCheckup } from '../../hooks/useGetFollowUpCheckup'
+
 const FollowUpCheckup = () => {
+
+    const { handleGetFollowUpCheckUps } = useGetFollowUpCheckup()
 
     return (
         <>
